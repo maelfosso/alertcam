30f58861b32f4db6892fe5c241716e5c0f950547
Mael FOSSO <fosso.mael.elvis@gmail.com>
2016-04-18 09:53:15 +0100

    First commit of AlertCam

---
+++ src/main/java/cm/cpc/alertcam/AlercamApplication.java
@@ -0,0 +1,12 @@
+package cm.cpc.alertcam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlercamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlercamApplication.class, args);
+	}
+}
---
+++ src/test/java/cm/cpc/alertcam/AlercamApplicationTests.java
@@ -0,0 +1,16 @@
+package cm.cpc.alertcam;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = AlercamApplication.class)
+public class AlercamApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
