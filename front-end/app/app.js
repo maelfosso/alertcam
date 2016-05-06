c5536ac447c770fb0fb245fc3f5318ffb836342c
Mael FOSSO <fosso.mael.elvis@gmail.com>
2016-05-06 17:18:58 +0100

    Sign, About US, Onctact Us front-end is OK

--- front-end/app/app.js
+++ front-end/app/app.js
@@ -1,5 +1,12 @@
-require('angular')
-var MainController = require('./controllers/MainController')
+'use strict';
 
-var app = angular.module('app', [])
-app.controller('MainController', ['$scope', MainController])
+// Declare app level module which depends on views, and components
+angular.module('myApp', [
+  'ngRoute',
+  'myApp.view1',
+  'myApp.view2',
+  'myApp.version'
+]).
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider.otherwise({redirectTo: '/view1'});
+}]);
